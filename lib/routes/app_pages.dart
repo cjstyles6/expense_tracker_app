1b13ac62815c529248b18cf9b0b2089a52f2fbc0
Chijioke Anazodo <chijiokeanazodo2020@gmail.com>
2024-11-20 22:33:43 -0800

    Update app_pages.dart

--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -3,6 +3,7 @@ import 'package:expense_tracker_app/features/add_expenses/screen/add_expenses_sc
 import 'package:expense_tracker_app/features/home/screen/home_screen.dart';
 import 'package:expense_tracker_app/features/home/screen/view_all_transactions_screen.dart';
 import 'package:expense_tracker_app/features/stats/screen/stats_screen.dart';
+import 'package:expense_tracker_app/features/welcome/screen/welcome_screen.dart';
 import 'package:expense_tracker_app/routes/app_routes.dart';
 import 'package:get/get.dart';
 
@@ -30,6 +31,12 @@ class AppPages {
       name: AppRoutes.TRANSACTIONS,
       page: () => const ViewAllTransactionsScreen(),
       binding: ExpenseBinding(),
+      // transition: Transition.noTransition,
+    ),
+    GetPage(
+      name: AppRoutes.ROUTES,
+      page: () => const WelcomeScreen(),
+      // binding: ExpenseBinding(),
       transition: Transition.noTransition,
     ),
   ];
